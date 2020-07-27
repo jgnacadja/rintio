22379c86a98c1fdf2aafba5959ee005f9367224f
lebel <lebel@localhost.localdomain>
2020-07-27 18:00:53 +0100

    creation de expert.vue

---
+++ src/components/Experts.vue
@@ -0,0 +1,147 @@
+<template>
+
+<h2> Nos experts<h2>
+
+<div class = "container"> 
+//Les quatres images sur la premiere ligne avec la figcaption
+
+<div class = "row">
+   
+    <div class= "col-md-3">
+    <figure>
+    <img src="/assets/experts/Mike.jpg" alt="" class = "rounded-circle"/>
+    <figcaption> 
+    Mike
+    <ul> 
+        <li> PhD Informatique </li>
+        <li> Data Architect, Cloud & DevOps Engineer </li>
+        <li> +10 Ans d’Expériences : AXA, Amadeus, Orange</li>
+    </ul>
+    </figcaption>
+    </figure>
+    <div>
+
+
+    <div class= "col-md-3">
+    <figure>
+    <img src="/assets/experts/Carlyna.jpg" alt="" class = "rounded-circle" />
+    <figcaption> 
+     Carlyna
+    <ul> 
+    <li> PhD Informatique </li>
+    <li>  Data Engineer </li>
+    <li>  +5 Ans d’Expériences : Ysance, Inria</li>
+    </ul>
+    </figcaption>
+    </figure>
+     <div>
+
+    <div class= "col-md-3">
+    <figure>
+    <img src="/assets/experts/Habib.jpg" alt="" class = "rounded-circle" />
+    <figcaption> 
+         Habib
+        <ul> 
+        <li> PhD Informatique </li>
+        <li>  Data Scientist , Project Manager </li>
+        <li>  +7 Ans d’Expériences : Ericsson,</li>
+        <li>  Orange, Bouygues Telecom</li>
+        </ul>
+    </figcaption>
+    </figure>
+    <div>
+
+     <div class= "col-md-3">
+    <figure>
+    <img src="/assets/experts/Talla.jpg" alt="" class = "rounded-circle" />
+    <figcaption> 
+      Talla
+        <ul> 
+        <li>  Ingénieur Informatique </li>
+        <li>  BI & Big Data Engineer, Project Manager </li>
+        <li>  +6 Ans d’Expériences : Orange, Oracle,
+            Société Générale</li>
+       
+        </ul>
+    </figcaption>
+    </figure>
+     <div>
+     
+    
+</div>
+
+//Les quatres images sur la seconde ligne avec les figcaption
+
+
+<div class = "row">
+   
+    <div class= "col-md-3">
+    <figure>
+    <img src="/assets/experts/julien.jpg" alt="" class = "rounded-circle" />
+    <figcaption> 
+     Julien
+        <ul> 
+        <li> Ingénieur Informatique </li>
+        <li>   Block Chain & DevOps Engineer</li>
+        <li>  +7 Ans d’Expériences : Ericsson,</li>
+       
+        </ul>
+    </figcaption>
+    </figure>
+    <div>
+
+
+    <div class= "col-md-3">
+    <figure>
+    <img src="/assets/experts/Fiacre.jpg" alt="" class = "rounded-circle" />
+    <figcaption> 
+     Fiacre
+        <ul> 
+        <li> Ingénieur Informatique, Actuaire </li>
+        <li>   Projet Manager, Scrum Master</li>
+        <li>   +7 Ans d’Exp : SNCF, AXA</li>
+        </ul>
+    </figcaption>
+    </figure>
+    <div>
+
+     <div class= "col-md-3">
+     <figure>
+    <img src="/assets/experts/Michel.jpg" alt="" class = "rounded-circle" />
+    <figcaption> 
+     Michel
+        <ul> 
+        <li>Ingénieur Informatique </li>
+        <li>    AI & Data Strategist, Enterprise Architect</li>
+        <li>  +10 Ans d’Expériences : Société Générale, BNP Paribas, Dassault, Naval Group</li>
+        </ul>
+    </figcaption>
+    </figure>
+    <div>
+
+     <div class= "col-md-3">
+     <figure>
+    <img src="/assets/experts/Nicaise.jpg" alt="" class = "rounded-circle" />
+    <figcaption>  Nicaise
+        <ul> 
+        <li>PhD Informatique </li>
+        <li>    Data Scientist, Data Architect</li>
+        <li> +8 Ans d’Expériences : Orange, Société Générale</li>
+        </ul></figcaption>
+    </figure>
+     <div>
+     
+    
+</div>
+
+
+
+
+
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
