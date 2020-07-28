41d4c51a2d757f0b973332ddfbb9e625f7092c03
lebel <lebel@localhost.localdomain>
2020-07-28 08:06:08 +0100

    Ajout de A propos

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,13 +2,26 @@
   <div>
     <Mainmenu />
     <transition name="fade" appear>
-      <main>
+      <main> 
+           <div class="container is-fluid">
+              <div class="has-text-link	">
+                <h1 class="title is-1 has-text-link">A propos</h1>
+              </div>
+            Rintio est une entreprise de services du numérique. Nous sommes spécialisés dans le développement et l’intégration de solution 
+            informatique, le traitement de données, les technologies Big Data et l’Intelligence Artificielle. Nous proposons entre autres: des 
+            prestations en Offshoring (Coding, Admin Infrastructure & Cloud, DevOps) pour nos clients en Europe et aux Etats Unis.
+       
+         </div>
         <slot />
       </main>
     </transition>
     <Footer />
   </div>
 </template>
+
+<style lang="css" scoped>
+
+</style>
 
 <script>
 import Mainmenu from "~/components/Mainmenu.vue";
