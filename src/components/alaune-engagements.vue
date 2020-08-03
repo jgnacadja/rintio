e70244d11daec6ad3a7f1b725d923c2f0940aafe
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2020-08-03 14:01:08 +0100

    cann't display articles

--- src/components/alaune-engagements.vue
+++ src/components/alaune-engagements.vue
@@ -1,8 +1,27 @@
 <template>
-  <Layout>
+  <div>
+      <div v-for="edge in $page.articles.edges" :key="edge.node.id">
+         <p>{{edge.node.title}}</p>
 
-    </Layout>
+      </div>
+  </div>
 </template>
+
+<page-query>
+query {
+  articles: allArticle {
+    edges {
+      node {
+        id
+        title
+        path
+      }
+    }
+  }
+}
+</page-query>
+
+
 
 <script>
 export default {
