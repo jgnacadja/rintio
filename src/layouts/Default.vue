36f8625497e95e468f53fc98540c398922aaec71
Lionel Okpeicha <lohnsonok@gmail.com>
2020-09-04 09:16:24 +0100

    fix: merge contact

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -12,9 +12,7 @@
 </style>
 
 <script>
-<<<<<<< HEAD
 import Mainmenu from "~/components/Mainmenu.vue";
-import Nosoffres from "~/components/Nosoffres.vue";
 import Footer from "~/components/Footer.vue";
 
 export default {
@@ -24,10 +22,5 @@ export default {
   }
 
 
-=======
-export default {
-  components: {
-  }
->>>>>>> contact
 };
 </script>
