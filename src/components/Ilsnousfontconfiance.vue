850beb8c95c8fb933a7cff105ea1a2278dbbdddf
Nazir12B <nazirgbadamassi24@gmail.com>
2020-09-07 16:30:14 +0100

    fix: fix bug on ilnousfontconfiance

--- src/components/Ilsnousfontconfiance.vue
+++ src/components/Ilsnousfontconfiance.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="newclass_ilsnousfontconfiance" @resize="listedelement()">
     <h2
-      class="container is-fluid title is-size-4-mobile is-size-2-tablet has-text-primary newclass_nostestimonials"
+      class="is-fluid title is-size-2 has-text-primary is-size-4-mobile is-size-2-tablet newclass_nostestimonials"
     >Ils nous font confiance</h2>
     <section id="wrap" class="columns is-mobile is-vcentered has-text-centered">
       <span class="column has-text-right" @click="roll('prev')">
@@ -258,6 +258,10 @@ export default {
 };
 </script>
 <style>
+.newclass_ilsnousfontconfiance h2{
+  margin-left:35px;
+  margin-top:35px;
+}
 #wrap {
   margin: 0;
   padding: 0;
