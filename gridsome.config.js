ee96a7f086cf5a33bbd06a96d6d31a4648291169
Lionel Okpeicha <lohnsonok@gmail.com>
2021-05-10 15:46:48 +0100

    fix: chainpack nodeExternals

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,8 +1,4 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const nodeExternals = require("webpack-node-externals");
 
 const tailwindcss = require("tailwindcss");
 
