5466f06b57bbb73ae11a46e45e3fa5e944e19812
Lionel Okpeicha <lohnsonok@gmail.com>
2021-06-04 11:32:31 +0100

    fix: add paginator

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -50,8 +50,14 @@
             </div>
           </div>
 
-          <!-- main post 1-->
-          <h2></h2>
+          <!-- paginator -->
+          <ul class="flex list-reset border border-grey-light rounded w-auto font-sans">
+            <li><a class="block hover:text-white hover:bg-blue text-blue border-r border-grey-light px-3 py-2" href="#">Previous</a></li>
+            <li><a class="block hover:text-white hover:bg-blue text-blue border-r border-grey-light px-3 py-2" href="#">1</a></li>
+            <li><a class="block hover:text-white hover:bg-blue text-blue border-r border-grey-light px-3 py-2" href="#">2</a></li>
+            <li><a class="block text-white bg-blue border-r border-blue px-3 py-2" href="#">3</a></li>
+            <li><a class="block hover:text-white hover:bg-blue text-blue px-3 py-2" href="#">Next</a></li>
+          </ul>
         </div>
       </div>
 
