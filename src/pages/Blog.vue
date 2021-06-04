5492f32b341ca3cfa0e4777fca5340d003208387
sylvère <akambi.sylv@gmail.com>
2021-06-04 15:55:53 +0100

    feat: Blog Post page

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -6,6 +6,7 @@
     <div
       class="md:flex container mx-auto md:px-16 space-x-0 md:space-x-6 w-full"
     >
+      
       <div id="home" class="container px-4 text-center w-3/4">
         <div class="space-y-6">
           <!-- main post -->
@@ -14,40 +15,42 @@
             v-for="edge in $page.posts.edges"
             :key="edge.node.id"
           >
-            <g-image
-              alt="iot"
-              src="~/assets/images/home/blog/iot.png"
-              class="object-cover w-full h-64 mb-0"
-            />
-            <span
-              class="relative text-white bg-secondary p-4 text-sm hidden md:block ml-4 -mt-7 capitalize w-24 text-center"
-            >
-              Big data
-            </span>
-            <p
-              class="text-gray-800 font-bold mt-2 mb-2 leading-tight text-xl mx-4"
-            >
-              {{ edge.node.title }}
-            </p>
-            <p class="text-primary text-xs space-x-8 mx-4">
-              <span>10 Jan 2020</span>
-              <span>Publié par : xxxxxxxxxxxxx</span>
-            </p>
-            <p class="text-gray-600 mb-4 mx-4">
-              Lorem Ipsum est simplement du faux texte employé dans la
-              composition et la mise en page avant impression...
-            </p>
-            <div class="flex space-x-8 mx-4">
-              <p class="flex space-x-2">
-                <Like /> <span class="text-xs">24k</span>
+            <g-link :to=edge.node.path>
+              <g-image
+                alt="iot"
+                src="~/assets/images/home/blog/iot.png"
+                class="object-cover w-full h-64 mb-0"
+              />
+              <span
+                class="relative text-white bg-secondary p-4 text-sm hidden md:block ml-4 -mt-7 capitalize w-24 text-center"
+              >
+                Big data
+              </span>
+              <p
+                class="text-gray-800 font-bold mt-2 mb-2 leading-tight text-xl mx-4"
+              >
+                {{ edge.node.title }}
               </p>
-              <p class="flex space-x-2">
-                <Comment /> <span class="text-xs">247</span>
+              <p class="text-primary text-xs space-x-8 mx-4">
+                <span>10 Jan 2020</span>
+                <span>Publié par : xxxxxxxxxxxxx</span>
               </p>
-              <p class="flex space-x-2">
-                <Share /> <span class="text-xs">24</span>
+              <p class="text-gray-600 mb-4 mx-4">
+                Lorem Ipsum est simplement du faux texte employé dans la
+                composition et la mise en page avant impression...
               </p>
-            </div>
+              <div class="flex space-x-8 mx-4">
+                <p class="flex space-x-2">
+                  <Like /> <span class="text-xs">24k</span>
+                </p>
+                <p class="flex space-x-2">
+                  <Comment /> <span class="text-xs">247</span>
+                </p>
+                <p class="flex space-x-2">
+                  <Share /> <span class="text-xs">24</span>
+                </p>
+              </div>
+            </g-link>
           </div>
 
           <!-- paginator -->
--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -1,10 +1,20 @@
 <template>
-  <div
-    id="home"
-    class="mx-auto text-center w-40 hover:border-b-8 border-secondary"
-  >
-    {{$page.post}}
-  </div>
+  <Layout>
+      <LazyHydrate when-idle>
+        <Breadcrumb :path="path" />
+      </LazyHydrate>
+
+      <div
+        class="md:flex container mx-auto md:px-16 space-x-0 md:space-x-6 w-full"
+      >
+        <div
+          id="home" class="container px-4 text-center w-3/4"
+        >
+          {{$page.post}}
+        </div>
+      </div>
+    
+  </Layout>
 </template>
 
 
@@ -14,6 +24,41 @@ query query($path:String = "") {
     id
     title
     path
+    published
+    categories {
+      id
+      title
     }
+    date
+    coverImage
+    metaDescription
+    subDescription
+    descriptionUp
+    descriptionDown
+  }
 }
 </page-query>
+
+
+<script>
+import LazyHydrate from "vue-lazy-hydration";
+import Breadcrumb from "~/components/Breadcrumb.vue";
+
+export default {
+  components: {
+    LazyHydrate,
+    Breadcrumb,
+  },
+  metaInfo: {
+    title: "Blog",
+  },
+  data() {
+    return {
+      path: "",
+    };
+  },
+  mounted() {
+    this.path = this.$router.currentRoute.path.slice(1).replace("-", " ");
+  },
+};
+</script>
