09058673fe5792f40b71de0be71b83dcd1218fec
Lionel Okpeicha <lohnsonok@gmail.com>
2021-06-21 09:27:08 +0100

    fix: 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+    <div class="h-screen w-full flex justify-center content-center flex-wrap">
+      <p class="font-sans text-black error-text">404</p>
+    </div>
+
+    <div
+      class="
+        absolute
+        w-full
+        bottom-4
+        mb-6
+        text-black text-center
+        font-sans
+        text-xl
+      "
+    >
+      <span class="opacity-50">Retour à </span>
+      <g-link class="border-b" to="/">l'acceuil</g-link>
+    </div>
+  </Layout>
+</template>
+
+<style scoped>
+.error-text {
+  font-size: 130px;
+}
+
+@media (min-width: 768px) {
+  .error-text {
+    font-size: 220px;
+  }
+}
+</style>>
