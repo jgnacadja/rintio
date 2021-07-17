d945fa2e8e7b1cc56a8a5a44ec6fb1ce78352024
Lionel Okpeicha <lohnsonok@gmail.com>
2021-07-17 13:14:31 +0100

    fix: footer bug

--- src/layouts/partials/Footer.vue
+++ src/layouts/partials/Footer.vue
@@ -12,7 +12,7 @@
         </div>
 
         <!-- Col-2 -->
-        <div class="-my-5 p-8 w-full sm:w-4/12 md:w-3/12 m-5">
+        <div class="-my-5 p-8 w-full sm:w-4/12 md:w-3/12 md:m-5">
           <!-- Col Title -->
           <div class="uppercase text-white font-extrabold mb-6 font-roboto">
             Produits
@@ -33,48 +33,26 @@
           <!-- Links -->
           <a
             href="https://coraq.formation.chmp.org"
-            class="
-              w-full
-              my-3
-              block
-              text-white
-              hover:text-gray-200
-              font-medium
-              duration-300
-            "
+            class="w-full my-3 block text-white hover:text-gray-200 font-medium duration-300"
           >
             &bull; Coraq Lab - CHMP
           </a>
           <a
             href="https://play.google.com/store/apps/details?id=com.rintio.idermato&hl=fr&gl=US"
-            class="
-              my-3
-              block
-              text-white
-              hover:text-gray-200
-              font-medium
-              duration-300
-            "
+            class="my-3 block text-white hover:text-gray-200 font-medium duration-300"
           >
             &bull; Idermato
           </a>
           <a
             href="#"
-            class="
-              my-3
-              block
-              text-white
-              hover:text-gray-300
-              font-medium
-              duration-300
-            "
+            class="my-3 block text-white hover:text-gray-300 font-medium duration-300"
           >
             &bull; Duniya Data
           </a>
         </div>
 
         <!-- Col-3 -->
-        <div class="-my-5 p-8 w-full sm:w-4/12 md:w-3/12 m-5">
+        <div class="-my-5 p-8 w-full sm:w-4/12 md:w-3/12 md:m-5">
           <!-- Col Title -->
           <div class="uppercase text-white font-extrabold mb-6 font-roboto">
             Liens utiles
@@ -95,161 +73,72 @@
           <!-- Links -->
           <a
             href="#"
-            class="
-              w-full
-              my-3
-              block
-              text-white
-              hover:text-gray-200
-              font-medium
-              duration-300
-            "
+            class="w-full my-3 block text-white hover:text-gray-200 font-medium duration-300"
           >
             &bull; Rintio Data Lab
           </a>
           <a
             href="#"
-            class="
-              my-3
-              block
-              text-white
-              hover:text-gray-200
-              font-medium
-              duration-300
-            "
+            class="my-3 block text-white hover:text-gray-200 font-medium duration-300"
           >
             &bull; Rintio Innovative Factory
           </a>
           <a
             href="#"
-            class="
-              my-3
-              block
-              text-white
-              hover:text-gray-300
-              font-medium
-              duration-300
-            "
+            class="my-3 block text-white hover:text-gray-300 font-medium duration-300"
           >
             &bull; Nos engagements
           </a>
           <a
             href="#"
-            class="
-              my-3
-              block
-              text-white
-              hover:text-gray-300
-              font-medium
-              duration-300
-            "
+            class="my-3 block text-white hover:text-gray-300 font-medium duration-300"
           >
             &bull; Africa Tech Up Tour
           </a>
         </div>
 
         <!-- Col-4 -->
-        <div class="-my-5 p-8 w-full sm:w-4/12 md:w-3/12 m-5">
+        <div class="-my-5 p-8 w-full sm:w-4/12 md:w-3/12 md:m-5">
           <div class="h-auto">
             <div class="text-red-light mb-3">Newsletter</div>
-            <div class=" responsive-component flex mt-4" v-res>
+            <div class="flex mt-4" v-res>
               <input
                 type="text"
-                class="
-                  p-2
-                  border border-grey-light
-                  round
-                  text-grey-dark text-sm
-                  h-auto
-                  rounded-l-sm
-                  text-gray-700
-                  focus:outline-none
-                "
+                class="w-full md:w-auto p-2 border border-grey-light round text-grey-dark text-sm h-auto rounded-l-sm text-gray-700 focus:outline-none"
                 placeholder="Votre adresse"
               />
               <button
-                class="
-                  bg-orange
-                  text-white
-                  rounded-r-sm
-                  h-auto
-                  text-xs
-                  p-3
-                  bg-secondary
-                  px-3
-                  py-2
-                  font-semibold
-                  focus:outline-none
-                "
+                class="bg-orange text-white rounded-r-sm h-auto text-xs p-3 bg-secondary px-3 py-2 font-semibold focus:outline-none"
               >
-               Je m'abonne
+                <span class="hidden md:block">Je m'abonne</span>
+                <Send class="md:hidden" />
               </button>
             </div>
             <p class="mt-6 mb-4">Suivez nous sur :</p>
             <div class="flex">
               <a
-                class="
-                  w-5
-                  h-5
-                  mr-2
-                  flex
-                  items-center
-                  place-content-center
-                  rounded-full
-                  bg-white
-                  text-primary text-xs
-                "
+                class="w-5 h-5 mr-2 flex items-center place-content-center rounded-full bg-white text-primary text-xs"
                 href="https://web.facebook.com/RintioGroup/?_rdc=1&_rdr"
                 target="_blank"
               >
                 <i class="fab fa-facebook-f"></i>
               </a>
               <a
-                class="
-                  w-5
-                  h-5
-                  mr-2
-                  flex
-                  items-center
-                  place-content-center
-                  rounded-full
-                  bg-white
-                  text-primary text-xs
-                "
+                class="w-5 h-5 mr-2 flex items-center place-content-center rounded-full bg-white text-primary text-xs"
                 href="https://www.linkedin.com/company/rintio/"
                 target="_blank"
               >
                 <i class="fab fa-linkedin-in relative"></i>
               </a>
               <a
-                class="
-                  w-5
-                  h-5
-                  mr-2
-                  flex
-                  items-center
-                  place-content-center
-                  rounded-full
-                  bg-white
-                  text-primary text-xs
-                "
+                class="w-5 h-5 mr-2 flex items-center place-content-center rounded-full bg-white text-primary text-xs"
                 href="https://twitter.com/RintioGroup/"
                 target="_blank"
               >
                 <i class="fab fa-twitter relative"></i>
               </a>
               <a
-                class="
-                  w-5
-                  h-5
-                  mr-2
-                  flex
-                  items-center
-                  place-content-center
-                  rounded-full
-                  bg-white
-                  text-primary text-xs
-                "
+                class="w-5 h-5 mr-2 flex items-center place-content-center rounded-full bg-white text-primary text-xs"
                 href="https://twitter.com/RintioGroup/"
                 target="_blank"
               >
@@ -273,11 +162,13 @@
 <script>
 import Logo from "./Logo";
 import Contact from "~/components/Contact.vue";
+import Send from "~/assets/images/icons/send.svg";
 
 export default {
   components: {
     Logo,
     Contact,
+    Send
   },
 };
 </script>
