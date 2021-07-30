1b2e06ae3525fd2465c41e841e9a573a8ddcca7e
lohnsonok <lohnsonok@gmail.com>
2021-07-30 09:22:51 +0100

    fix: error build

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -5,11 +5,9 @@
     </LazyHydrate>
 
     <div
-
-      class="container w-full mx-auto mt-8 md:mt-24 space-x-0 md:flex md:px-16 md:space-x-6 "
-
+      class="container w-full mx-auto mt-8 md:mt-24 space-x-0 md:flex md:px-16 md:space-x-6"
     >
-      <div id="home" class="container w-full ">
+      <div id="home" class="container w-full">
         <div class="mx-4 md:px-10">
           <h3 class="md:mb-12 text-lg text-primary font-bold">
             Articles à la une
@@ -22,48 +20,46 @@
         >
           <div
             id="slide_item"
-            class="absolute px-10 pb-10 inset-0 bg-transparent flex items-end transition-all ease-in-out duration-1000 transform translate-x-0 slide "
+            class="absolute px-10 pb-10 inset-0 bg-transparent flex items-end transition-all ease-in-out duration-1000 transform translate-x-0 slide"
           >
-
             <div class="mx-16 md:my-16">
-              <div class=" md:my-8 text-lg text-primary font-bold pt-4">
-                  Remise des diplômes au Africa TechUp Tour 
-                  <br>Abidjan 2021
+              <div class="md:my-8 text-lg text-primary font-bold pt-4">
+                Remise des diplômes au Africa TechUp Tour
+                <br />Abidjan 2021
               </div>
               <p class="mt-8 md:my-8 text-gray-600">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Suspendisse lacus interdum quis sociis vitae tempor volutpat. 
-                Elit faucibus arcu, amet, praesent. Laoreet fringilla amet 
-                tellus ultrices facilisi egestas maecenas. Integer cras tristique urna,
-                diam, sed enim. Egestas vehicula ipsum vitae amet a nam lacinia.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse lacus interdum quis sociis vitae tempor volutpat.
+                Elit faucibus arcu, amet, praesent. Laoreet fringilla amet
+                tellus ultrices facilisi egestas maecenas. Integer cras
+                tristique urna, diam, sed enim. Egestas vehicula ipsum vitae
+                amet a nam lacinia.
               </p>
               <div class="fixed h-full bg-white">
-                    <div class="fixed text-left mx-4 md:px-10">
-                        <h3 class="md:mb-12 ml-2 text-sm text-primary font-bold pt-3">
-                            Rintio 
-                              <p class="text-sm mb-2 text-gray-300">
-                                17 Mai 2021
-                              </p>
-                         </h3>
-                     </div>
+                <div class="fixed text-left mx-4 md:px-10">
+                  <h3 class="md:mb-12 ml-2 text-sm text-primary font-bold pt-3">
+                    Rintio
+                    <p class="text-sm mb-2 text-gray-300">17 Mai 2021</p>
+                  </h3>
+                </div>
                 <g-image
-                    src="~/assets/images/Blog/article1.png"
-                    alt=""
-                    class="pt-2 w-auto">
+                  src="~/assets/images/Blog/article1.png"
+                  alt=""
+                  class="pt-2 w-auto"
+                >
                 </g-image>
               </div>
             </div>
             <div class="h-5/6 md:h-full my-px md:flex-shrink-0">
-               <g-image
-                 src="~/assets/images/Blog/certificate1.png"
-                 alt=""
-                 class=" h-96 pt-5 pb-5 w-auto">
-               </g-image>
+              <g-image
+                src="~/assets/images/Blog/certificate1.png"
+                alt=""
+                class="h-96 pt-5 pb-5 w-auto"
+              >
+              </g-image>
             </div>
-
           </div>
           <div
-
             id="slide_item"
             class="absolute px-10 pb-10 inset-0 bg-transparent flex items-end justify-center transition-all ease-in-out duration-1000 transform translate-x-full slide"
           >
@@ -76,7 +72,6 @@
           <div
             id="slide_item"
             class="absolute px-10 pb-10 inset-0 bg-transparent flex items-end justify-center transition-all ease-in-out duration-1000 transform translate-x-full slide"
-
           >
             <p
               class="bg-gray-900 px-7 py-3 text-white font-bold uppercase rounded-md opacity-80"
@@ -85,18 +80,16 @@
             </p>
           </div>
         </div>
-        <div class="flex float-right mt-4" style="margin-right: 3.58rem ">
+        <div class="flex float-right mt-4" style="margin-right: 3.58rem">
           <div
             class="z-10 bg-primary bg-opacity-10 w-12 h-12 mr-2 rounded-md border border-primary flex items-center justify-center text-black cursor-pointer"
             @click="previousSlide()"
           >
-
             <ArrowLeft />
           </div>
           <div
             class="z-10 bg-primary bg-opacity-10 w-12 h-12 rounded-md border border-primary flex items-center justify-center text-black cursor-pointer"
             @click="nextSlide()"
-
           >
             <ArrowRight />
           </div>
@@ -104,26 +97,19 @@
       </div>
     </div>
 
-<LazyHydrate when-visible>
-
-      <Article :allposts="$page.allOffer.edges" :type="'offer'" />
+    <LazyHydrate when-visible>
+      <Carrousel :allposts="$page.allOffer.edges" :type="'offer'" />
     </LazyHydrate>
 
     <LazyHydrate when-visible>
-
-      <Article :allposts="$page.allPosts.edges" :type="'post'" />
-    </LazyHydrate>
-
-    <LazyHydrate when-visible>
-
-      <Post />
+      <Carrousel :allposts="$page.allPosts.edges" :type="'post'" />
     </LazyHydrate>
   </Layout>
 </template>
 
 <page-query>
 query {
-  posts:  allBlogPost {
+  posts: allBlogPost {
     edges {
       node {
         id
@@ -133,7 +119,7 @@ query {
           id
           title
         }
-      tags {
+        tags {
           id
           title
         }
@@ -149,7 +135,7 @@ query {
   }
   tags: allTag {
     edges {
-      node{
+      node {
         id
         title
       }
@@ -157,14 +143,14 @@ query {
   }
   category: allCategory {
     edges {
-      node{
+      node {
         id
         title
       }
     }
   }
-  
-  onlinePost : allBlogPost(
+
+  onlinePost: allBlogPost(
     filter: { categories: { in: "Blog" } }
     limit: 3
     order: DESC
@@ -184,7 +170,7 @@ query {
     }
   }
 
-    allPosts: allBlogPost(filter: { categories: { in: "Blog" } }, order: DESC) {
+  allPosts: allBlogPost(filter: { categories: { in: "Blog" } }, order: DESC) {
     edges {
       node {
         id
@@ -217,45 +203,33 @@ query {
       }
     }
   }
-
 }
-
-
 </page-query>
 
-
 <script>
-import Offer from "~/components/Blog/Offer.vue"
+import Offer from "~/components/blog/Offer.vue";
 import moment from "moment";
 import LazyHydrate from "vue-lazy-hydration";
-import Breadcrumb from "~/components/Breadcrumb.vue";
-import { Pager } from "gridsome";
-import Seo from "~/assets/images/Illustrations/seo.svg";
 import RemoveTag from "~/assets/images/icons/remove.svg";
 import ArrowLeft from "~/assets/images/icons/blog-arrow-left.svg";
 import ArrowRight from "~/assets/images/icons/blog-arrow-right.svg";
 import Keywords from "~/assets/keywords.json";
-import BreadcrumbBlog from "~/components/Blog/BreadcrumbBlog.vue";
-import Event from "~/components/Blog/Event.vue";
-import Articles from '../components/Blog/Articles.vue';
-import Article from '../components/Blog/Article.vue';
-//import Post from "~/components/blog/Post.vue";
+import Breadcrumb from "~/components/blog/Breadcrumb.vue";
+import Event from "~/components/blog/Event.vue";
+import Carrousel from "../components/blog/Carrousel.vue";
 
 export default {
   components: {
     Offer,
     LazyHydrate,
     Breadcrumb,
-    Pager,
-    Seo,
     RemoveTag,
     ArrowLeft,
     ArrowRight,
-    BreadcrumbBlog,
+    Breadcrumb,
     Event,
-    Article,
-    //Post
-    Articles  },
+    Carrousel,
+  },
   metaInfo() {
     return {
       title: "Blog",
@@ -278,11 +252,6 @@ export default {
   data() {
     return {
       path: "",
-      search: "",
-      searchByTag: "",
-      page: 1,
-      numberOfPages: 1,
-      pinnedTabs: [],
       config: {
         keywords: [
           "service",
@@ -311,39 +280,6 @@ export default {
   mounted() {
     this.path = this.$router.currentRoute.path.slice(1).replace("-", " ");
   },
-  computed: {
-    searchResults() {
-      if (this.search) {
-        this.pinnedTabs = [];
-        let filteredPosts = this.$page.posts.edges.filter((post) => {
-          return post.node.title
-            .toLowerCase()
-            .includes(this.search.toLowerCase().trim());
-        });
-        this.numberOfPages = Math.ceil(filteredPosts.length / 5);
-        return this.paginate(filteredPosts, 5, this.page);
-      } else {
-        console.log(this.$page.posts.edges);
-        if (this.pinnedTabs.length !== 0) {
-          console.log(this.pinnedTabs);
-
-          /* // Get all the required ids
-          var ids = filter.map(function (f) {
-            return f.id;
-          });
-          var filtered = array.filter(function (a) {
-            // Check if both source and target are present in list of ids
-            return ids.indexOf(a.source) !== -1 && ids.indexOf(a.target) !== -1;
-          }); */
-
-          console.log(filtered);
-          return [];
-        } else {
-          return this.paginate(this.$page.posts.edges, 5, this.page);
-        }
-      }
-    },
-  },
   methods: {
     paginate(array, page_size, page_number) {
       // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
@@ -351,14 +287,6 @@ export default {
         (page_number - 1) * page_size,
         page_number * page_size
       );
-    },
-    pinTag(tag) {
-      if (this.pinnedTabs.indexOf(tag) !== -1) {
-        this.pinnedTabs = this.pinnedTabs.filter((tagged) => tagged != tag);
-      } else {
-        this.search = "";
-        this.pinnedTabs.push(tag);
-      }
     },
     nextSlide() {
       if (this.currentSlide == 3) {
@@ -419,44 +347,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-@import "../variables.scss";
-
-.pager {
-  display: inline-block;
-  width: 100%;
-  text-align: center;
-  padding: 1rem;
-
-  &__link {
-    color: #e5e7eb;
-    text-align: center;
-    text-decoration: none;
-
-    &:hover:not(.active) {
-      background-color: #9ca3af;
-      color: #000 !important;
-    }
-  }
-}
-
-.pager a {
-  background-color: #fff;
-  width: 2rem;
-  padding: 1rem;
-  border: 1px solid rgb(223, 223, 223);
-}
-
-.pager a:hover {
-  background-color: #f9fafb;
-  color: #000 !important;
-}
-
-.active {
-  background-color: #e5e7eb !important;
-  color: #000 !important;
-  padding: 1rem;
-  border: 1px solid #3b82f6 !important;
-}
-</style>
