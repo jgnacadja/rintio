755d9e828f46056800ccee63eff7bcb1212c694d
Adio7 <adioyaya6@gmail.com>
2021-08-03 16:24:43 +0100

    article-blog

--- src/components/blog/Carrousel.vue
+++ src/components/blog/Carrousel.vue
@@ -18,7 +18,7 @@
             font-bold
           "
         >
-          <span v-if="type === 'post'">À la une</span>
+          <span class="text-3xl" v-if="type === 'post'">À la une</span>
           <span v-if="type === 'offer'">Offres</span>
           <span v-if="type === 'event'">Evènements</span>
         </h3>
