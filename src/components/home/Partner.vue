ed4673c30478104c178b22654f0b689580d24a4e
Adio7 <adioyaya6@gmail.com>
2021-08-18 18:15:42 +0100

    fix-partner

--- src/components/home/Partner.vue
+++ src/components/home/Partner.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="md:px-16 md:flex w-full justify-center py-6 md:py-24 text-black"
+    class="md:px-16 md:flex w-full justify-center flex-wrap py-6 md:py-24 text-black"
     style="background: #e8e8e8"
   >
-    <div class="w-full md:w-full lg:w-1/4 xl:w-1/4 text-center md:text-center lg:text-left">
+    <div class="w-full md:w-full lg:w-1/4 text-center md:text-center lg:text-left">
       <div class="mx-4 md:mx-0">
         <h3 class="mb-3 text-sm text-primary font-semibold uppercase">
           Nos partenaires
@@ -46,47 +46,47 @@
 
     <div
       class="
-        w-full md:w-full lg:w-3/4 xl:3/4
+        w-full md:w-full lg:w-3/4 
         flex-row
         justify-center
         items-start
         flex-wrap
         mx-auto
         flex
-        space-x-4
+        lg:space-x-4
       "
     >
       <div class="1/3 space-y-4">
         <div
-          class="rounded-xl md:rounded-3xl p-2 m-2 bg-white text-gray-100 w-12 md:w-72 h-12 md:h-72 flex items-center justify-center text-xl font-bold shadow-xl"
+          class="rounded-xl md:rounded-3xl p-2 m-2 bg-white text-gray-100 w-12 md:w-48 lg:w-48 xl:w-72 h-12 md:h-48 lg:h-48 xl:h-72 flex items-center justify-center text-xl font-bold shadow-xl"
         >
           <Studely />
         </div>
         <div
-          class="rounded-xl md:rounded-3xl md:ml-auto p-2 m-2 bg-white text-yellow-100 w-12 md:w-40 h-12 md:h-40 flex items-center justify-center text-xl font-bold shadow-xl"
+          class="rounded-xl md:rounded-3xl md:ml-auto p-2 m-2 bg-white text-yellow-100 w-12 md:w-28 lg:w-32 xl:w-40 h-12 md:h-28 lg:h-32 xl:h-40 flex items-center justify-center text-xl font-bold shadow-xl"
         >
           <Hacklab />
         </div>
       </div>
       <div class="1/3 space-y-4 ">
         <div
-          class="rounded-xl md:rounded-3xl md:ml-auto p-2 m-2 bg-white text-indigo-100 w-12 md:w-64 h-12 md:h-64 flex items-center justify-center text-xl font-bold shadow-xl"
+          class="rounded-xl md:rounded-3xl md:ml-auto p-2 m-2 bg-white text-indigo-100 w-12 md:w-44 lg:w-40 xl:w-64 h-12 md:h-44 lg:h-40 xl:h-64 flex items-center justify-center text-xl font-bold shadow-xl"
         >
           <Bnpparibas />
         </div>
         <div
-          class="rounded-xl md:rounded-3xl md:ml-auto p-2 m-2 bg-white text-green-100 w-12 md:w-60 h-12 md:h-60 flex items-center justify-center text-xl font-bold shadow-xl"
+          class="rounded-xl md:rounded-3xl md:ml-auto p-2 m-2 bg-white text-green-100 w-12 md:w-40 lg:w-32 xl:w-60 h-12 md:h-40 lg:h-32 xl:h-60 flex items-center justify-center text-xl font-bold shadow-xl"
         >
           <CiviTime />
         </div>
-        <div class="lg:flex">
+        <div class="md:flex">
           <div
-            class="rounded-xl md:rounded-3xl md:ml-auto p-2 m-2 bg-white text-purple-100 w-12 md:w-24 h-12 md:h-24  flex items-center justify-center text-xl font-bold shadow-xl"
+            class="rounded-xl md:rounded-3xl lg:ml-auto p-2 m-2 bg-white text-purple-100 w-12 md:w-20 lg:w-16 xl:w-24 h-12 md:h-20 lg:h-16 xl:h-24 flex items-center justify-center text-xl font-bold shadow-xl"
           >
             <Isci />
           </div>
           <div
-            class="rounded-xl md:rounded-3xl md:ml-auto p-2 m-2 bg-white text-red-100 w-12 md:w-24 h-12 md:h-24 flex items-center justify-center text-xl font-bold shadow-xl"
+            class="rounded-xl md:rounded-3xl xl:ml-auto p-2 m-2 bg-white text-red-100 w-12 md:w-20 lg:w-16 xl:w-24 h-12 md:h-20 lg:h-16 xl:h-24 flex items-center justify-center text-xl font-bold shadow-xl"
           >
             <Initiative5 />
           </div>
@@ -94,24 +94,24 @@
       </div>
       <div class="1/3 space-y-4">
         <div
-          class="rounded-xl md:rounded-3xl md:ml-auto p-2 m-2 bg-white text-pink-100 w-12 md:w-full h-12 md:h-72 flex items-center justify-center text-xl font-bold shadow-xl"
+          class="rounded-xl md:rounded-3xl xl:ml-auto p-2 m-2 bg-white text-pink-100 w-12 md:w-56 lg:w-60 xl:w-full h-12 md:h-40 lg:h-36 xl:h-60 flex items-center justify-center text-xl font-bold shadow-xl"
         >
           <Societegeneral />
         </div>
-        <div class="lg:flex">
+        <div class="md:flex">
           <div
-            class="rounded-xl md:rounded-3xl p-2 m-2 bg-white text-blue-100 w-12 md:w-32 h-12 md:h-32 flex items-center justify-center text-xl font-bold shadow-xl"
+            class="rounded-xl md:rounded-3xl p-2 m-2 bg-white text-blue-100 w-12 md:w-24 lg:w-24 xl:w-32 h-12 md:h-24 lg:h-24 xl:h-32 flex items-center justify-center text-xl font-bold shadow-xl"
           >
             <Actudata />
           </div>
           <div
-            class="rounded-xl md:rounded-3xl p-2 m-2 md:ml-auto bg-white text-orange-100 w-12 md:w-32 h-12 md:h-32 flex items-center justify-center text-xl font-bold shadow-xl"
+            class="rounded-xl md:rounded-3xl p-2 m-2 xl:ml-auto bg-white text-orange-100 w-12 md:w-24 lg:w-24 xl:w-32 h-12 md:h-24 lg:h-24 xl:h-32 flex items-center justify-center text-xl font-bold shadow-xl"
           >
             <Compta360 />
           </div>
         </div>
         <div
-          class="rounded-xl md:rounded-3xl p-2 m-2 md:ml-auto bg-white text-teal-100 w-12 md:w-64 h-12 md:h-64 flex items-center justify-center text-xl font-bold shadow-xl"
+          class="rounded-xl md:rounded-3xl p-2 m-2 lg:ml-auto bg-white text-teal-100 w-12 md:w-56 lg:w-52 xl:w-64 h-12 md:h-56 lg:h-52 xl:h-64 flex items-center justify-center text-xl font-bold shadow-xl"
         >
           <ExpertiseFrance />
         </div>
