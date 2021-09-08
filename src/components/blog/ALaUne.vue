7907d8fabbd7a8f34376064eeee02ad920a4384f
Adio7 <adioyaya6@gmail.com>
2021-09-08 12:26:54 +0100

    update-blog

--- src/components/blog/ALaUne.vue
+++ src/components/blog/ALaUne.vue
@@ -17,57 +17,59 @@
           class="border rounded shadow-sm bg-white"
         >
           <div v-for="edge in featuredPost" :key="edge.node.id">
-            <div
+            <g-link :to="edge.node.path">
+              <div
               class="grid grid-cols-1 md:grid-cols-2 md:px-8 md:gap-x-8 md:py-4"
-            >
-              <div
-                class="col-start-1 row-start-2 mt-72 md:mt-0 md:row-start-1 px-4"
               >
-                <h2
-                  class="text-lg md:text-2xl text-left font-bold text-secondary text-opacity-70 py-3 mt-24 md:mt-0"
-                >
-                  {{ edge.node.title }}
-                </h2>
-                <p
-                  class="py-3 text-justify"
-                  v-html="richtextToHTML(edge.node.metaDescription)"
-                ></p>
                 <div
-                  class="flex items-center text-sm font-medium my-5 md:mt-2 md:mb-4"
+                  class="col-start-1 row-start-2 mt-72 md:mt-0 md:row-start-1 px-4"
                 >
-                  <div class="ml-1">
-                    <img
-                      :src="edge.node.coverImage.file.url"
-                      alt=""
-                      class="rounded-full inset-0 w-12 h-12 object-cover bg-gray-100"
-                    />
-                  </div>
+                  <h2
+                    class="text-lg md:text-2xl text-left font-bold text-secondary text-opacity-70 py-3 mt-24 md:mt-0"
+                  >
+                    {{ edge.node.title }}
+                  </h2>
+                  <p
+                    class="py-3 text-justify"
+                    v-html="richtextToHTML(edge.node.metaDescription)"
+                  ></p>
+                  <div
+                    class="flex items-center text-sm font-medium my-5 md:mt-2 md:mb-4"
+                  >
+                    <div class="ml-1">
+                      <img
+                        :src="edge.node.coverImage.file.url"
+                        alt=""
+                        class="rounded-full inset-0 w-12 h-12 object-cover bg-gray-100"
+                      />
+                    </div>
 
-                  <div class="flex flex-col px-4">
-                    <p class="text-primary text-base font-medium">
-                      {{ edge.node.author }}
-                    </p>
-                    <p class="text-gray-400 -mt-6">
-                      {{ edge.node.date | FormatDate }}
-                    </p>
+                    <div class="flex flex-col px-4">
+                      <p class="text-primary text-base font-medium">
+                        {{ edge.node.author }}
+                      </p>
+                      <p class="text-gray-400 -mt-6">
+                        {{ edge.node.date | FormatDate }}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div
-                class="col-start-1 row-start-1 flex md:col-start-2 md:row-span-3"
-              >
-                <div class="w-full grid grid-cols-1 grid-rows-2 gap-2">
-                  <div class="relative col-span-3 row-span-2 md:col-span-2">
-                    <img
-                      :src="edge.node.coverImage.file.url"
-                      alt=""
-                      class="absolute inset-0 w-full md:h-full h-96 object-cover bg-gray-100 md:rounded-lg"
-                    />
+                <div
+                  class="col-start-1 row-start-1 flex md:col-start-2 md:row-span-3"
+                >
+                  <div class="w-full grid grid-cols-1 grid-rows-2 gap-2">
+                    <div class="relative col-span-3 row-span-2 md:col-span-2">
+                      <img
+                        :src="edge.node.coverImage.file.url"
+                        alt=""
+                        class="absolute inset-0 w-full md:h-full h-96 object-cover bg-gray-100 md:rounded-lg"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              </div>              
+            </g-link>
           </div>
         </VueSlickCarousel>
         <div class="flex float-right mt-4" style="margin-right: 2rem">
