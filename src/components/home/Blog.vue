76447bdde1ff6939989d685a8c07e1b91f35b501
Adio7 <adioyaya6@gmail.com>
2021-09-30 18:52:58 +0100

    optimize-width-height

--- src/components/home/Blog.vue
+++ src/components/home/Blog.vue
@@ -40,12 +40,14 @@
         :key="edge.node.id"
       >
         <g-link :to="edge.node.path">
-          <g-image
-            alt="iot"
-            title="scrum"
-            :src="edge.node.coverImage.file.url"
-            class="object-cover w-full h-64 mb-0"
-          />
+          <div class="sm:aspect-w-16 sm:aspect-h-6 md:aspect-w-16 md:aspect-h-8 xl:aspect-w-16 xl:aspect-h-8">
+            <g-image
+              alt="iot"
+              title="scrum"
+              :src="edge.node.coverImage.file.url"
+              class="object-cover w-full h-64 mb-0"
+            />
+          </div>
 
           <h5
             class="mx-4 mt-2 mb-2 text-xl font-bold leading-tight text-gray-800"
@@ -93,12 +95,14 @@
         <div class="flex flex-row h-full">
           <div class="w-2/5 h-48 md:h-full">
             <g-link :to="edge.node.path">
-              <g-image
-                alt="iot"
-                title="scrum"
-                :src="edge.node.coverImage.file.url"
-                class="object-cover w-full h-full"
-              />
+              <div class="sm:aspect-w-16 sm:aspect-h-10 md:aspect-w-14 md:aspect-h-16 xl:aspect-w-16 xl:aspect-h-16">
+                <g-image
+                  alt="iot"
+                  title="scrum"
+                  :src="edge.node.coverImage.file.url"
+                  class="object-cover w-full h-full"
+                />
+              </div>
             </g-link>
           </div>
           <div class="w-3/5 px-4 mt-4">
