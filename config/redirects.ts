1ef155e83898a6e7ba66e972b69c46a5b06d388b
ADISSA <nimathadissa@gmail.com>
2026-08-05 11:53:25 +0200

    fix: [CLIENT#000000] translating coment

--- config/redirects.ts
+++ config/redirects.ts
@@ -3,6 +3,8 @@ export interface LegacyRedirect {
   to: string
 }
 
+// Add legacy URLs here (renamed slugs, moved articles, etc.)
+// to 301-redirect to their current URL.
 export const legacyRedirects: LegacyRedirect[] = [
   { from: '/blog/article/africa-tech-up-tour-atut', to: '/blog/article/africa-techup-tour-atut' }
 ]
